c744f40142c3458b71ff6212095fb085c0f83b81
Djibi DIOP <ddiopdjibi@gmail.com>
2019-05-05 18:10:37 +0200

    create services heros and message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,23 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import {FormsModule} from '@angular/forms';
 import { HeroDetailComponent } from './heroes/hero-detail/hero-detail.component';
+import {HeroService} from './heroes/hero.service';
+import { MessagesComponent } from './heroes/messages/messages.component';
+import {MessageService} from './message.service';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [HeroService, MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
